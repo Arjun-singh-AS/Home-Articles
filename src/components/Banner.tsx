dca843566af587aefa1909b14c4dee8d4b56ef22
Arjun-singh-AS <arjunsinghsarvaniya2003@gmail.com>
2024-10-23 13:45:42 +0530

    new8

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 const Banner = () => {
@@ -12,9 +12,6 @@ const Banner = () => {
   const bannerRef = useRef<HTMLDivElement | null>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false); // Track hover state
-
-  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0); // Explicitly type currentIndex as a number
-  const [selectedImage,setSelectedImage]=useState(0)
 
   useEffect(() => {
     if (!isHovered) { // Only change images when not hovered
@@ -47,93 +44,8 @@ const Banner = () => {
   }, [currentIndex, images.length]);
 
   
-
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) => 
-      (prevIndex - 1 + images.length) % images.length
-    );
-  };
-
-  const handleImage=(index:SetStateAction<number>)=>{
-    setSelectedImage(index)
-  }
   return (
     <>
-
-<div className="container mx-auto px-4">
-      {/* For large devices, show products in a grid */}
-      <div className="hidden lg:grid grid-cols-4 gap-6">
-        <div>
-          {images.map((image, index) => (
-            <div
-              key={index}
-              
-            >
-              <button onClick={() => handleImage(index)} className={`${
-                selectedImage === index ? 'border-4 border-blue-500' : ''
-              }`}>
-                <Image
-                  src={image} // dynamic image from the map
-                  alt={`Product ${index}`}
-                  width={100} // optimized width
-                  height={100} // optimized height
-                  objectFit="cover" // maintain aspect ratio
-                  className="rounded-md shadow-sm m-2"
-                />
-              </button>
-            </div>
-          ))}
-        </div>
-
-        {/* Large screen selected image */}
-        <div>
-          <Image
-            src={images[selectedImage]}
-            alt="Selected Product"
-            width={450}
-            height={600}
-            objectFit="cover"
-            className="rounded-md shadow-sm"
-          />
-        </div>
-      </div>
-
-      {/* For small devices, show a swipeable carousel */}
-      <div className="lg:hidden flex items-center justify-center overflow-hidden relative">
-        {/* Previous Button */}
-        <button
-          onClick={prevImage}
-          className="absolute left-0 z-10 p-2 text-white bg-gray-800 rounded-full"
-        >
-          &#10094; {/* Left arrow */}
-        </button>
-
-        {/* Display the current image */}
-        <div className="snap-center shrink-0 w-full">
-          <Image
-            src={images[currentImageIndex]}
-            alt={`Product ${currentImageIndex}`}
-            width={450}
-            height={600}
-            objectFit="cover"
-            className="rounded-md shadow-sm"
-          />
-        </div>
-
-        {/* Next Button */}
-        <button
-          onClick={nextImage}
-          className="absolute right-0 z-10 p-2 text-white bg-gray-800 rounded-full"
-        >
-          &#10095; {/* Right arrow */}
-        </button>
-      </div>
-    </div>
-
 
       <div
         ref={bannerRef}
