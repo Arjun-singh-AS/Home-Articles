0312bb3462dbadbc440b676ad88131f9e416c8c5
Arjun-singh-AS <arjunsinghsarvaniya2003@gmail.com>
2024-11-01 14:10:45 +0530

    meta5

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default async function sitemap():Promise<MetadataRoute.Sitemap>{
     }))
     return [
         {
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}/`
+            url:`${process.env.NEXT_PUBLIC_BASE_URL}/about`
         },
         ...productsEntries
     ]
